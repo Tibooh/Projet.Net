7953f1cb0ff69fe368b26d4334974145c6213532
tibooh <thibaut.pernet@gmail.com>
2017-03-02 09:34:13 +0100

    first commit

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Odbc;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        List<Product> products = new List<Product>();
+
+
+        public List<Product> GetAllProducts()
+        {
+            try
+            {
+                using (OdbcConnection connection = new OdbcConnection(ConfigurationManager.ConnectionStrings["MySQLConnStr"].ConnectionString))
+                {
+                    connection.Open();
+                    using (OdbcCommand command = new OdbcCommand("SELECT name FROM test_users", connection))
+                    using (OdbcDataReader dr = command.ExecuteReader())
+                    {
+                        while (dr.Read())
+                            products.Add(new Product { Id = (int)dr["id"], Name = (string)dr["name"], Category = (string)dr["country"], Price = 1 });
+                        dr.Close();
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ ProductsApp/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Odbc;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class UserController : ApiController
+    {
+        User[] users = new User[]
+       {
+            new User { Id = 1, Name = "Tomato Soup", Country = "Groceries" },
+            new User { Id = 2, Name = "Yo-yo", Country = "Toys" },
+            new User { Id = 3, Name = "Hammer", Country = "Hardware" }
+       };
+
+        public IEnumerable<User> GetAllUser()
+        {
+            try
+            {
+                using (OdbcConnection connection = new OdbcConnection(ConfigurationManager.ConnectionStrings["MySQLConnStr"].ConnectionString))
+                {
+                    connection.Open();
+                    using (OdbcCommand command = new OdbcCommand("SELECT name FROM test_users", connection))
+                    using (OdbcDataReader dr = command.ExecuteReader())
+                    {
+                        while (dr.Read())
+                            dr.Close();
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return users;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = users.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
